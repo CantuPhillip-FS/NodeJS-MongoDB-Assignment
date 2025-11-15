04ee0f29537ac3a01f32f8bb1e8b13ce91c7313a
hereisphil <thereisphil@gmail.com>
2025-11-15 16:43:42 -0600

    SERVER: User model, 1st iteration, done.

--- Server/app/models/User.ts
+++ Server/app/models/User.ts
@@ -1,6 +1,30 @@
 import { Schema, model } from "mongoose";
 
-const userSchema = new Schema({}, { timestamps: true });
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "User first name is required."],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "User last name is required."],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "User email is required"],
+      unique: [true, "User with email already exists. Please login."],
+      trim: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+        "Email must match this format: email@email.com",
+      ],
+    },
+  },
+  { timestamps: true }
+);
 
 const UserModel = model("User", userSchema);
 export default UserModel;
