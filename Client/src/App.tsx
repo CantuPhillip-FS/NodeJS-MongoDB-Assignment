447d26496cf16d864d24dfb47c4eccae581652a0
hereisphil <thereisphil@gmail.com>
2025-11-16 20:46:50 -0600

    CLIENT: Per Brad's video created a SearchBar.tsx and imported it with proper props and a handleSearch function in App.tsx. Now I'll attempt to setup fetch Animes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import ListAllusers from "./components/ListAllUsers";
+import SearchBar from "./components/SearchBar";
 import SignupForm from "./components/SignupForm";
 
 function App() {
+  // this state is used to properly re-fetch the users upon new signups
   const [reloadUsers, setReloadUsers] = useState(0);
+
+  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Search button clicked >>>", e.target.search.value);
+  };
   return (
     <main>
       <h1>Hello World</h1>
+      <SearchBar onSubmit={handleSearch} />
       <SignupForm onSignup={() => setReloadUsers((state) => state + 1)} />
       <ListAllusers reloadUsers={reloadUsers} />
     </main>
